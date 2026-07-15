1181d6d14c2574bb5ccff31ebf043d779408cf1c
Sridhar <>
2026-07-15 13:56:40 +0530

    ConfigDynamicAPIUrl

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@
  */
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 
 export const api = axios.create({ baseURL: API_BASE });
 
